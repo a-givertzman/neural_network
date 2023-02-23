6a1e6605d2ec4c2dc24b98f543fbc4fa95b743c6
a-givertzman <a.givertzman@icloud.com>
2023-02-23 15:05:35 +0300

    Matrix

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use crate::lib::matrix::Matrix;
 
 fn main() {
     let m = Matrix {rows: 4, cols: 4, data: vec![vec![0.0; 4]; 4]};
-    println!("m 4x4: {:?}", m);
+    println!("Matrix 4x4: {:?}", m);
+    let mr = Matrix::rendom(4, 4);
+    println!("Matrix ranom 4x4: {:?}", mr);
 }
