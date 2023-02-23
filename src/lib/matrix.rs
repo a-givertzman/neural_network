e730d5cc2dbc7c6f09c85f77db40ac4971f39462
a-givertzman <a.givertzman@icloud.com>
2023-02-23 17:56:41 +0300

    Matrix

--- src/lib/matrix.rs
+++ src/lib/matrix.rs
@@ -1,6 +1,9 @@
+#![allow(non_snake_case)]
 use rand::Rng;
 
-#[derive(Debug)]
+
+
+#[derive(Debug, Clone)]
 pub struct Matrix {
     pub rows: usize,
     pub cols: usize,
@@ -18,15 +21,110 @@ impl Matrix {
         }
     }
     ///
+    /// creates Matrix from Vec<Vec<f64>>
+    pub fn from(data: Vec<Vec<f64>>) -> Matrix {
+        Matrix { 
+            rows: data.len(), 
+            cols: data.first().unwrap().len(), 
+            data,
+        }
+    }
+    ///
     /// matrix filled with random values
     pub fn rendom(rows: usize, cols: usize) -> Matrix {
         let mut rng = rand::thread_rng();
-        let mut m = Matrix::zeros(rows, cols);
-        for rowIndex in 0..(rows -1) {
-            for colIndex in 0..(cols -1) {
-                m.data[rowIndex][colIndex] = rng.gen::<f64>() * 2.0 -1.0;
+        Matrix{
+            rows, 
+            cols,
+            data: (0..rows).map(|_| {
+                (0..cols).map(|_| {
+                    // let res = (rowIndex as f64) + (colIndex as f64) / 10.0;
+                    // println!("rowIndex: {}, colIndex: {} | {:?}", rowIndex, colIndex, res);
+                    rng.gen::<f64>() * 2.0 -1.0
+                }).collect()
+            }).collect(),
+        }
+    }
+    ///
+    /// multyplies a self by other
+    pub fn multiply(&mut self, other: &Matrix) -> Matrix {
+        if self.cols != other.rows {
+            panic!("Inposible to multiply by matrix of incorrect dimesions" );
+        }
+        let mut res = Matrix::zeros(self.rows, other.cols);
+        for rowIndex in 0..self.rows {
+            for colIndex in 0..other.cols {
+                let mut sum = 0.0;
+                for index  in 0..self.cols {
+                    sum += self.data[rowIndex][index] * other.data[index][colIndex];
+                }
+                res.data[rowIndex][colIndex] = sum;
             }
         }
-        m
+        res
+    }
+    ///
+    /// adds a self and other
+    pub fn add(&mut self, other: &Matrix) -> Matrix {
+        if self.cols != other.cols || self.rows != other.rows {
+            panic!("Inposible to add matrix of incorrect dimesions" );
+        }
+        let mut res = Matrix::zeros(self.rows, other.cols);
+        for rowIndex in 0..self.rows {
+            for colIndex in 0..self.cols {
+                res.data[rowIndex][colIndex] = self.data[rowIndex][colIndex] + other.data[rowIndex][colIndex];
+            }
+        }
+        res
+    }
+    ///
+    /// dot multiplies self by other matrix
+    pub fn dotMultiply(&mut self, other: &Matrix) -> Matrix {
+        if self.cols != other.cols || self.rows != other.rows {
+            panic!("Inposible to dot multiply by matrix of incorrect dimesions" );
+        }
+        let mut res = Matrix::zeros(self.rows, other.cols);
+        for rowIndex in 0..self.rows {
+            for colIndex in 0..self.cols {
+                res.data[rowIndex][colIndex] = self.data[rowIndex][colIndex] * other.data[rowIndex][colIndex];
+            }
+        }
+        res
+    }
+    ///
+    /// subtracts a self and other
+    pub fn subtract(&mut self, other: &Matrix) -> Matrix {
+        if self.cols != other.cols || self.rows != other.rows {
+            panic!("Inposible to subtract matrix of incorrect dimesions" );
+        }
+        let mut res = Matrix::zeros(self.rows, other.cols);
+        for rowIndex in 0..self.rows {
+            for colIndex in 0..self.cols {
+                res.data[rowIndex][colIndex] = self.data[rowIndex][colIndex] - other.data[rowIndex][colIndex];
+            }
+        }
+        res
+    }
+    ///
+    /// Maps all element of a matrix
+    pub fn map(&mut self, function: &dyn Fn(f64) -> f64) -> Matrix {
+        Matrix::from(
+            (self.data).clone().into_iter().map(|row| {
+                row.into_iter().map(|value| {
+                    function(value)
+                }).collect()
+            }).collect(),
+        )
+    }
+    ///
+    /// transposes a matrix
+    pub fn transpose(&mut self) -> Matrix {
+        let mut res = Matrix::zeros(self.cols, self.rows);
+        for i in 0..self.rows {
+            for j in 0..self.cols {
+                res.data[j][i] = self.data[i][j];
+            }
+        }
+        res
     }
 }
